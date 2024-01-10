8dc7f27df1bb090b0d4c29fb90e2c6772337b525
FreddleSpl0it <patschul@posteo.de>
2024-01-10 20:59:26 +0100

    [Web] fix sogo redirect

--- data/conf/sogo/custom-sogo.js
+++ data/conf/sogo/custom-sogo.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     var loginForm = document.forms.namedItem("loginForm");
     if (loginForm) {
-        window.location.href = '/user';
+        window.location.href = '/';
     }
 
     angularReady = false;
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
             const toolbarElement = document.body.querySelector('.md-toolbar-tools.sg-toolbar-group-last.layout-align-end-center.layout-row');
 
-            var htmlCode = '<a class="md-icon-button md-button md-ink-ripple" aria-label="mailcow" href="/" aria-hidden="false" tabindex="-1">' +
+            var htmlCode = '<a class="md-icon-button md-button md-ink-ripple" aria-label="mailcow" href="/user" aria-hidden="false" tabindex="-1">' +
                 '<md-icon class="material-icons" role="img" aria-label="arrow_back">arrow_back</md-icon>' +
                 '</a><a class="md-icon-button md-button md-ink-ripple" aria-label="mailcow" href="#" onclick="logout.submit()" aria-hidden="false" tabindex="-1">' +
                 '<md-icon class="material-icons" role="img" aria-label="settings_power">settings_power</md-icon>' +
