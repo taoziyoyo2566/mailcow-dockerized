c39839024b68544837d0ca34bf614766cfffb088
FreddleSpl0it <patschul@posteo.de>
2024-01-10 12:57:57 +0100

    [Web] create sogo webmail login

--- data/web/sogo.php
+++ data/web/sogo.php
@@ -8,7 +8,7 @@ if (isset($_SESSION['mailcow_cc_role']) && $_SESSION['mailcow_cc_role'] == 'user
   // load master password
   $sogo_sso_pass = file_get_contents("/etc/sogo-sso/sogo-sso.pass");
   // register username and password in session
-  $_SESSION[$session_var_user_allowed][] = $login;
+  $_SESSION[$session_var_user_allowed][] = $_SESSION['mailcow_cc_username'];
   $_SESSION[$session_var_pass] = $sogo_sso_pass;
 
   header("Location: /SOGo/so/{$_SESSION['mailcow_cc_username']}");
