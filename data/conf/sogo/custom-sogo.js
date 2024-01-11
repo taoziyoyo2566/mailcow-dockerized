518d2b430cd28e27f76df448ffab0bd49285d986
FreddleSpl0it <patschul@posteo.de>
2024-01-11 14:02:40 +0100

    [Web] change sogo back link button icon

--- data/conf/sogo/custom-sogo.js
+++ data/conf/sogo/custom-sogo.js
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', function () {
             const toolbarElement = document.body.querySelector('.md-toolbar-tools.sg-toolbar-group-last.layout-align-end-center.layout-row');
 
             var htmlCode = '<a class="md-icon-button md-button md-ink-ripple" aria-label="mailcow" href="/user" aria-hidden="false" tabindex="-1">' +
-                '<md-icon class="material-icons" role="img" aria-label="arrow_back">arrow_back</md-icon>' +
+                '<md-icon class="material-icons" role="img" aria-label="build">build</md-icon>' +
                 '</a><a class="md-icon-button md-button md-ink-ripple" aria-label="mailcow" href="#" onclick="logout.submit()" aria-hidden="false" tabindex="-1">' +
                 '<md-icon class="material-icons" role="img" aria-label="settings_power">settings_power</md-icon>' +
                 '</a><form action="/" method="post" id="logout"><input type="hidden" name="logout"></form>';
